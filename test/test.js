7b8f952c6a572421ef6701168330fb027fccad62
Nicholas <nikolay.spiridonov@gmail.com>
2016-02-08 15:22:30 +0700

    :white_check_mark: Test

---
+++ test/test.js
@@ -0,0 +1,8 @@
+var wt = require('./index.js')
+var assert = require('assert')
+
+describe('WiredTiger', function() {
+    it('Should throw error', function() {
+        assert.throws(wt, new Error);
+    })
+})
